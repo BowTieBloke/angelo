889e0ac1318f40bb95ba5ddc4a421dcd096dd2c6
Simon Reinsch <gruenfink1111@googlemail.com>
2025-06-09 00:00:37 +0200

    use a theme

--- app/src/main/java/de/arschwasser/angelo/ui/theme/Type.kt
+++ app/src/main/java/de/arschwasser/angelo/ui/theme/Type.kt
@@ -6,10 +6,11 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.googlefonts.Font
 
+
 val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
     providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs
+    certificates = emptyList()
 )
 
 val bodyFontFamily = FontFamily(
