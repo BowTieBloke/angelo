10a63a9cafc60571665f7cf01b1f8213b38cc530
Simon Reinsch <gruenfink1111@googlemail.com>
2025-06-24 00:13:16 +0200

    downgrade android api version

--- app/src/main/java/de/arschwasser/angelo/core/AppConfig.kt
+++ app/src/main/java/de/arschwasser/angelo/core/AppConfig.kt
@@ -7,7 +7,7 @@ object AppConfig {
     var CONNECT_TIMEOUT_MS = 3_000
     var READ_TIMEOUT_MS = 8_000
     var QR_URL_PATTERN = "\${BASE_URL}/game?c={code}"
-    const val SPOTIFY_CLIENT_ID = "d3dcbf1f102c4b968f8c69c97c22f7f2"
-    const val SPOTIFY_REDIRECT_URI = "angelo://auth"
+//    const val SPOTIFY_CLIENT_ID = "d3dcbf1f102c4b968f8c69c97c22f7f2"
+//    const val SPOTIFY_REDIRECT_URI = "angelo://auth"
     var ALLOW_SCREEN_FLASH = true
 }
